42016c3302b41592997f3408b42c9b215a6915a6
AlphaOmega301 <78671140+AlphaOmega301@users.noreply.github.com>
2021-02-22 19:13:00 +0900

    Update Ifexample1.java
    Chang Kor to Eng

--- javapractice/First/src/chapter4/Ifexample1.java
+++ javapractice/First/src/chapter4/Ifexample1.java
@@ -5,10 +5,10 @@ public class Ifexample1 {
 	public static void main(String[] args) {
 		int age = 7;
 		if(age >= 8) {
-			System.out.println("학교에 다닙니다.");
+			System.out.println("Go School.");
 		}
 		else {
-			System.out.println("학교에 다니지 않습니다.");
+			System.out.println("Too young to go to school");
 		}
 
 	}
