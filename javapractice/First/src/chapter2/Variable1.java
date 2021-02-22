3cdc3ffaa70abc7ddbbfcda9c78289163d2eb341
AlphaOmega301 <dpsalpha9319@gmail.com>
2021-02-22 19:08:56 +0900

    Change in Variable1.java. age cal.

--- javapractice/First/src/chapter2/Variable1.java
+++ javapractice/First/src/chapter2/Variable1.java
@@ -5,6 +5,15 @@ public class Variable1 {
 	public static void main(String[] args) {
 		final int MY_AGE = 22;
 		System.out.println(MY_AGE);
+		int AN_AGE;
+		for(AN_AGE = 23; AN_AGE<=50; AN_AGE++){
+			if(AN_AGE%12==0){
+				System.out.println("12 times someting age is" + AN_AGE);
+			}
+			else{
+				continue;
+			}
+		}
 		
 	}
 
